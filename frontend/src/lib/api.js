7d558e46325c69419a930f7a007830c89a442fb1
Mochamad Adam Fikri <mochamad.adamfikri@gmail.com>
2026-09-22 14:44:29 +0700

    fix: use same-origin API by default

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
+  baseURL: process.env.REACT_APP_BACKEND_URL
+    ? `${process.env.REACT_APP_BACKEND_URL.replace(/\/$/, "")}/api`
+    : "/api",
   withCredentials: true,
 });
 
